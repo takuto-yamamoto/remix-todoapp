c8d94feafa677c158338b36d6a4ea881b21f1d9c
takuto-yamamoto <takuto.yamamoto.dev@gmail.com>
2025-02-28 02:16:37 +0900

    fix: remix contacts app

---
+++ apps/frontend/app/remix.config.ts
@@ -0,0 +1,9 @@
+import { AppConfig } from '@remix-run/dev';
+
+const config: AppConfig = {
+  future: {
+    v3_singleFetch: true,
+  },
+};
+
+export default config;
--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -1,12 +1,35 @@
 import {
+  Form,
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react';
+import type { LinksFunction } from '@remix-run/node';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+import appStylesHref from './app.css';
+import { createEmptyContact, getContacts } from './data';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: appStylesHref },
+];
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return { contacts };
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return { contact };
+};
+
+export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -16,14 +39,56 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                aria-label="Search contacts"
+                id="q"
+                name="q"
+                placeholder="Search"
+                type="search"
+              />
+              <div aria-hidden hidden={true} id="search-spinner" />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{' '}
+                      {contact.favorite ? <span>★</span> : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+        </div>
+
+        <div id="detail">
+          <Outlet />
+        </div>
+
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
